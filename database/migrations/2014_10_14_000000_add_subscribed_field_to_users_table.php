78d8ace02f27f14557aff76061023ec9c4f09d50
Dan Richards <danrichardsri@gmail.com>
2015-09-19 06:18:22 -0400

    Added migration for subscribed status and removed some unneeded views

---
+++ database/migrations/2014_10_14_000000_add_subscribed_field_to_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('email_history', function(Blueprint $table)
+        {
+            $table->engine = 'InnoDB';
+            $table->increments('id', true);
+            $table->integer('user_id')->nullable();
+            $table->string('token', 23);
+            $table->string('recipient', 50);
+            $table->string('name', 255);
+            $table->string('subject', 255);
+            $table->mediumText('data')->nullable();
+            $table->string('job', 255);
+            $table->string('view', 255);
+            $table->dateTime('expire')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('email_history');
+    }
+}
--- resources/views/simples/sample-violation.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Danimal
- * Date: 9/14/2015
- * Time: 9:15 PM
- */
